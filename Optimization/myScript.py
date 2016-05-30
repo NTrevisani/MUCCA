902fbd2826cbd6ccd4c4f5cd1958a9af9f30ebd2
NTrevisa <n.trevisani@campus.unimib.it>
2016-05-30 10:15:52 +0200

    script for reading

---
+++ Optimization/myScript.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import time
+
+print sys.argv
+if len(sys.argv) <= 1 :
+    print "please tell me which file you want me to analyze: \n"
+    exit()
+
+#input variables
+os.system('root -l -b -q \'mymacros/variables.C(\"' + sys.argv[1] + '.root\")\'')
+#correlation matrix
+os.system('root -l -b -q \'mymacros/correlations.C(\"' + sys.argv[1] + '.root\")\'')
+#ROC curve
+os.system('root -l -b -q \'mymacros/efficiencies.C(\"' + sys.argv[1] + '.root\")\'')
+os.system('root -l -b -q \'mymacros/mvaeffs.C(\"' + sys.argv[1] + '.root\")\'')
+#check overtraining
+os.system('root -l -b -q \'mymacros/mvas.C(\"' + sys.argv[1] + '.root\",0)\'')
+os.system('root -l -b -q \'mymacros/mvas.C(\"' + sys.argv[1] + '.root\",1)\'')
+os.system('root -l -b -q \'mymacros/mvas.C(\"' + sys.argv[1] + '.root\",2)\'')
+os.system('root -l -b -q \'mymacros/mvas.C(\"' + sys.argv[1] + '.root\",3)\'') #test/training superimposed
+
+os.system('rm -rf plots' + sys.argv[1] + '/')
+os.system('mv plots plots' + sys.argv[1] + '/')
+
+os.system('mkdir ~/www/figuresLxplus/')
+os.system('mkdir ~/www/figuresLxplus/' + time.strftime("%d%b%Y") + '/')
+os.system('mkdir ~/www/figuresLxplus/' + time.strftime("%d%b%Y") + '/monoH')
+os.system('mkdir ~/www/figuresLxplus/' + time.strftime("%d%b%Y") + '/monoH/TMVA')
+os.system('mkdir ~/www/figuresLxplus/' + time.strftime("%d%b%Y") + '/monoH/TMVA/plots' + sys.argv[1])
+
+os.system('cp ~/www/index.php plots' + sys.argv[1] + '/* ~/www/figuresLxplus/' + time.strftime("%d%b%Y") + '/monoH/TMVA/plots' + sys.argv[1])
+
