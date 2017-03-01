de93344dcc7b6b3e26bab894b76aebbf2e26e42d
NTrevisa <n.trevisani@campus.unimib.it>
2017-03-01 12:53:38 +0100

    updating scripts

--- Optimization/trainAll.py
+++ Optimization/trainAll.py
@@ -1,10 +1,12 @@
 import os
 
 nvariables=['0','1','2','3','4','5']
+channel=['em','sf','ll']
 
 for bkg in range(1,7) :
     for model in range (1,3) :
         for index in range(len(nvariables)) :
-            print("Weights-2HDM_TTbar_0var")
-            print("bsub -q 1nh -o jobs/ submit-train.sh " + str(bkg) + " " + str(model) + " \\\"" + nvariables[index] + "\\\"")
-            os.system("bsub -q 1nh -o jobs/ submit-train.sh " + str(bkg) + " " + str(model) + " \\\"" + nvariables[index] + "\\\"")
+            for indexCh in range(len(channel)) :
+                print("Weights-2HDM_TTbar_0var_em")
+                print("bsub -q 1nh -o jobs/ submit-train.sh " + str(bkg) + " " + str(model) + " \\\"" + nvariables[index] + "\\\" \\\"" + channel[indexCh] + "\\\"")
+                os.system("bsub -q 1nh -o jobs/ submit-train.sh " + str(bkg) + " " + str(model) + " \\\"" + nvariables[index] + "\\\" \\\"" + channel[indexCh] + "\\\"")
